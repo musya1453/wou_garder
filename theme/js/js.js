176469ea6a386138384461e9b923b56760b4734d
Musaev Ruslan <musya.wow@gmail.com>
2016-02-15 08:22:04 +0300

    done

--- theme/js/js.js
+++ theme/js/js.js
@@ -1,26 +1,61 @@
 (function ($) {
 
-  if (typeof Drupal != 'undefined') {
-    Drupal.behaviors.wcportalMobileTheme = {
-      attach: function (context, settings) {
-        init();
-      },
+  $(window).load(function () {
+    // The slider being synced must be initialized first
+    $('#carousel').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      directionNav: false,
+      itemWidth: 95,
+      itemMargin: 6,
+      asNavFor: '#slider'
+    });
 
-      completedCallback: function () {
-        // Do nothing. But it's here in case other modules/themes want to override it.
-      }
-    }
-  }
-
-  $(function () {
-    if (typeof Drupal == 'undefined') {
-      init();
-    }
+    $('#slider').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      directionNav: false,
+      slideshow: false,
+      sync: "#carousel"
+    });
   });
 
-  function init() {
+  $(window).load(function () {
+    var btn = $('.btn-menu'),
+      $menu = $('.nav');
 
-  }
+    btn.on('click touch', function (e) {
+      e.preventDefault();
+
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $menu.hide();
+      } else {
+        $(this).addClass('active');
+        $menu.show();
+      }
+    })
+  });
+
+  $(window).load(function(){
+    var btn = $('.btn-smile'),
+      smiles = $('.smile-container');
+
+    btn.on('click touch', function (e) {
+      e.preventDefault();
+
+      if($(this).hasClass('active')){
+        $(this).removeClass('active');
+        smiles.hide();
+      }else{
+        $(this).addClass('active');
+        smiles.show();
+      }
+    })
+  })
 
 })(jQuery);
 
